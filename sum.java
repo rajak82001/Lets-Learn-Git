3bc73b9e99ba5fab4b9e78e2a174c9f258aee335
Raja khan <rajak82001@gmail.com>
2023-04-01 15:59:06 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        int c = num1+num2;
+        System.out.println("sum of two number is: "+c);
+
+    }
+
+}
